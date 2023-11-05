6e960d6f0194985511e4b0da591db950390783f1
JennySeidenschwarz <jenny.seidenschwarz@web.de>
2023-11-05 12:52:26 +0100

    const icp before redo

--- 3DOpenWorldMOT/models/constrained_icp.py
+++ 3DOpenWorldMOT/models/constrained_icp.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib
+import os
+import copy
 import open3d as o3
 import numpy as np
 import torch
@@ -9,7 +14,9 @@ class ConstrainedICPRegistration():
         self.active_tracks = active_tracks
         self.av2_loader = av2_loader
         self.min_pts_thresh = min_pts_thresh
+        self.log_id = log_id
         self.registration_len_thresh = registration_len_thresh
+        self.ordered_timestamps = av2_loader.get_ordered_log_lidar_timestamps(log_id)
 
     def load_pointclouds(self, pt1, pt2, return_numpy=False):
         pc1_centroid = pt1.mean(axis=0)
@@ -27,18 +34,26 @@ class ConstrainedICPRegistration():
         init[:3, 3] = approx_translation
         return init
     
-    def get_grid_init(self, translation, lwh):
+    def get_grid_init(self, translation, lwh, alpha_2, alpha_1):
         inits = list()
+        # print('------------------> A1 A2', alpha_2, alpha_1)
+        c = np.cos(alpha_2-alpha_1)
+        s = np.sin(alpha_2-alpha_1)
         for prod_l in [-0.5, -0.25, 0, 0.25, 0.5]:
             for prod_w in [-0.5, -0.25, 0, 0.25, 0.5]:
-                inits.append(translation-lwh*np.array(prod_l, prod_w, 1))
-        
+                init = np.eye(4)
+                # init[0, 0] = c
+                # init[0, 1] = -s
+                # init[1, 0] = s
+                # init[1, 1] = c
+                init[:3, 3] = lwh*np.array([prod_l, prod_w, 0])
+                inits.append(init)
         return inits
 
-    def icp_p2point(self, pc1, pc2, translation, lwh, radius=0.2, its=30, init=None, with_constraint=True):
+    def icp_p2point(self, pc1, pc2, translation, lwh, alpha_2, alpha_1, radius=0.2, its=30, init=None, with_constraint=True):
         pc1, pc2, pc1_centroid = self.load_pointclouds(pc1, pc2)
         # init = self.get_centroid_init(pc1, pc2)
-        inits = self.get_grid_init(translation, lwh)
+        inits = self.get_grid_init(translation, lwh, alpha_2, alpha_1)
         inlier_rmse = 100000000
         reg_p2p = None
         for init in inits:
@@ -49,8 +64,24 @@ class ConstrainedICPRegistration():
                 init,
                 o3.TransformationEstimationPointToPoint(with_constraint=with_constraint, with_scaling=False),
                 o3.registration.ICPConvergenceCriteria(max_iteration=its))
+            # print(init, _reg_p2p.inlier_rmse, inlier_rmse,  _reg_p2p.transformation)
+            if  np.asarray(_reg_p2p.correspondence_set).shape[0] == 0:
+                continue
+            # print('ye')
             if _reg_p2p.inlier_rmse < inlier_rmse:
+                # print(init, _reg_p2p.inlier_rmse, inlier_rmse,  _reg_p2p.transformation)
+                # print()
                 reg_p2p = _reg_p2p
+                inlier_rmse = _reg_p2p.inlier_rmse 
+        if reg_p2p is None:
+            reg_p2p = o3.registration_icp(
+                pc1,
+                pc2,
+                radius,
+                np.eye(4),
+                o3.TransformationEstimationPointToPoint(with_constraint=with_constraint, with_scaling=False),
+                o3.registration.ICPConvergenceCriteria(max_iteration=its))
+        
         pc1.transform(reg_p2p.transformation)
         rotation = reg_p2p.transformation[:-1, :-1]
         translation = reg_p2p.transformation[:-1, -1]
@@ -59,24 +90,26 @@ class ConstrainedICPRegistration():
     def register(self, max_interior_thresh=50):
         detections = dict()
         for j, track in enumerate(self.active_tracks.values()):
-            #print(len(track), self.registration_len_thresh, self.min_pts_thresh, track.max_num_interior, track.min_num_interior, len(track) > self.registration_len_thresh and track.min_num_interior >= self.min_pts_thresh)
             track_dets = list()
+            _start = list()
+            _end = list()
+            dets = copy.deepcopy(track.detections)
             # we start from timestep with most points and then go
             # from max -> end -> start -> max
             max_interior_idx = torch.argmax(torch.tensor([d.num_interior for d in track.detections])).item()
             max_interior = torch.max(torch.tensor([d.num_interior for d in track.detections])).item()
             max_interior_pc = torch.atleast_2d(track._get_canonical_points(i=max_interior_idx))
-            start_in_t0 = max_interior_pc
+            max_lwh = track.detections[max_interior_idx].lwh
+            print(len(track) > self.registration_len_thresh and track.min_num_interior >= self.min_pts_thresh, len(track), self.registration_len_thresh, track.min_num_interior, self.min_pts_thresh)
             if len(track) > self.registration_len_thresh and track.min_num_interior >= self.min_pts_thresh:
                 max_to_end = range(max_interior_idx, len(track)-1)
-                end_to_start = range(len(track)-1, 0, -1)
-                start_to_max = range(0, max_interior_idx)
-                iterators = [max_to_end, end_to_start, start_to_max]
-                increment = [+1, -1, +1]
+                max_to_start = range(max_interior_idx, 0, -1)
+                iterators = [max_to_end, max_to_start]
+                increment = [+1, -1]
 
                 SimilarityTransforms = [None] * len(track)
                 for iterator, increment in zip(iterators, increment):
-                    start_in_t0 = start_in_t0
+                    start_in_t0 = copy.deepcopy(max_interior_pc)
                     for i in iterator:
                         # convert pc and trajectory t0 --> t1 from ego frame t=i to ego frame t=i+1
                         t0 = track.detections[i].timestamps[0, 0].item()
@@ -92,32 +125,58 @@ class ConstrainedICPRegistration():
                             start_in_t1,
                             cano1_in_t1,
                             track.detections[i+increment].translation,
-                            track.detections[i+increment].lwh)
-                        track.detections[i+increment].update_after_registration(torch.from_numpy(start_registered), translation, rotation)
-                        '''else:
-                            traj_in_t0 = torch.atleast_2d(track._get_traj(i=i))
-                            t0 = track.detections[i].timestamps[0, 0].item()
-                            t1 = track.detections[i+1].timestamps[0, 0].item()
-                            dt = self.ordered_timestamps.index(t1) - self.ordered_timestamps.index(t0)
-                            translation_1 = start_in_t0 + traj_in_t0[:, dt].mean(dim=0)
-                            translation_1 = track._convert_time(t0, t1, self.av2_loader, translation_1).mean(dim=0)
-                            translation_2 = torch.atleast_2d(track._get_canonical_points(i=i+1)).mean(dim=0)
-                            init_T = (
-                                translation_2 - translation_1).float().cuda().unsqueeze(0)
-                            init_R = torch.tensor([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]]).float().cuda()
-                            init_s = torch.tensor([1]).float().cuda()
-                            init = pytorch3d.ops.points_alignment.SimilarityTransform(
-                                R=init_R, T=init_T, s=init_s)
-                            # if point cloud small just use distance as transform and assume no rotation
-                            start_registered = pytorch3d.ops.points_alignment._apply_similarity_transform(
-                                    start_in_t1.cuda().float().unsqueeze(0),
-                                    init.R,
-                                    init.T,
-                                    init.s).squeeze()
-                            start_registered = torch.atleast_2d(start_in_t0.cpu().squeeze())'''
-
+                            track.detections[i+increment].lwh,
+                            track.detections[i+increment].alpha,
+                            track.detections[i].alpha)
+                        _start.append(cano1_in_t1)
+                        _end.append(start_registered)
+                        print(track.detections[i+increment].translation, track.detections[i+increment].lwh, track.detections[i+increment].alpha)
+                        #print('lwh', track.detections[i+increment].lwh)
+                        #print('trans', track.detections[i+increment].translation)
+                        #print()
+                        track.detections[i+increment].update_after_registration(torch.from_numpy(start_registered), translation, rotation, max_lwh)
+                        print(track.detections[i+increment].translation, track.detections[i+increment].lwh, track.detections[i+increment].alpha)
+                        #print(track.detections[i+increment].lwh)
+                        #print(track.detections[i+increment].translation)
+                        #quit()
                         # concatenate cano points at t+increment and registered points as
                         # point cloud for next timestamp / final point cloud
                         start_in_t0 = start_registered # torch.cat([start_registered, cano1_in_t1])
-                
+                self.visualize(dets, track.detections, track.track_id, max_interior_pc, self.log_id, _start, _end)            
+                print(len(dets), len(track.detections))
+                quit()
+            # track.fill_detections(self.av2_loader, self.ordered_timestamps, max_time=25)
+            detections[j] = track.detections
         return detections
+
+    def visualize(self, dets, registered_dets, track_id, start_in_t0, log_id, _start, _end):
+        os.makedirs('/workspace/3DOpenWorldMOT_motion_patterns/3DOpenWorldMOT/3DOpenWorldMOT/Visualization_reg', exist_ok=True)
+        fig, ax = plt.subplots()
+        for start_in_t0 in _start:
+            plt.scatter(start_in_t0[:, 0], start_in_t0[:, 1], color='green', s=2)
+        for start_in_t0 in _end:
+            plt.scatter(start_in_t0[:, 0], start_in_t0[:, 1], color='pink', s=2)
+
+        for det in dets:
+            self.add_patch(ax, det)
+
+        for det in registered_dets:
+            self.add_patch(ax, det, color='blue', add=10)
+        ax.axis('equal')
+        plt.savefig(
+                f'/workspace/3DOpenWorldMOT_motion_patterns/3DOpenWorldMOT/3DOpenWorldMOT/Visualization_reg/frame_{track_id}_{log_id}.jpg', dpi=1000)
+        plt.close()
+        
+    def add_patch(self, ax, det, color='black', add=0):
+        plt.scatter(det.translation[0], det.translation[1], color=color, marker='o', s=2)
+        loc_0 = det.translation[:-1]-0.5*det.lwh[:-1]
+        t = matplotlib.transforms.Affine2D().rotate_around(det.translation[0], det.translation[1], det.alpha) + ax.transData
+        rect = patches.Rectangle(
+            loc_0,
+            det.lwh[0],
+            det.lwh[1],
+            linewidth=1,
+            edgecolor=color,
+            facecolor='none',
+            transform=t)
+        ax.add_patch(rect)
